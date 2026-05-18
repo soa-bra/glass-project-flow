4f29eab76ecbb93ec9cd6570d0a09c1a5907730b
SoaBra <soabrago@gmail.com>
2026-05-19 00:22:48 +0300

    Unify archive category contracts, sources, and acceptance checks

--- src/components/ArchivePanel/categories/HRArchivePanel.tsx
+++ src/components/ArchivePanel/categories/HRArchivePanel.tsx
@@ -1,7 +1,1 @@
-import React from 'react';
-import { GenericArchiveCategoryPanel } from './GenericArchiveCategoryPanel';
-import { ARCHIVE_MOCK_DATA } from './archiveData';
-
-export const HRArchivePanel: React.FC = () => (
-  <GenericArchiveCategoryPanel category="hr" records={ARCHIVE_MOCK_DATA.hr} />
-);
+export { HRArchivePanel } from './archiveCategoryPanels';
