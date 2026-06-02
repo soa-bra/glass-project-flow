518fda35a5be2707c4f6639b340e4bcde980a6bc
SoaBra <soabrago@gmail.com>
2026-06-03 02:34:23 +0300

    test(planning): fix UUID matcher

--- src/features/planning/state/slices/planningElementIds.test.ts
+++ src/features/planning/state/slices/planningElementIds.test.ts
@@ -4,7 +4,7 @@ import { createElementsSlice } from './elementsSlice';
 import { createToolsSlice } from './toolsSlice';
 import { createPenSlice } from './penSlice';
 
-const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i;
+const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 type TestState = ReturnType<typeof createElementsSlice> &
   ReturnType<typeof createToolsSlice> &
