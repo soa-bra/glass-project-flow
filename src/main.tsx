a9c287d45d283068847922b15698d124ad7f9fc0
SoaBra <soabrago@gmail.com>
2026-06-22 11:15:32 +0300

    fix(ci): remove unused helmet provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
 import './index.css'
 import { Telemetry } from './infra/telemetry'
@@ -9,8 +8,6 @@ Telemetry.init();
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 );
