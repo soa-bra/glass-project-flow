478b1a34fada0fc5c1d520bd823a780f0d1c4746
SoaBra <soabrago@gmail.com>
2026-06-25 21:19:51 +0300

    Preserve overview grid order on tablet

--- src/components/OperationsBoard/Overview/OverviewGrid.tsx
+++ src/components/OperationsBoard/Overview/OverviewGrid.tsx
@@ -25,7 +25,6 @@ export const OverviewGrid: React.FC = () => {
       <AppDashboardGrid
         columns={12}
         density="default"
-        autoFlow="dense"
         minRowHeight="120px"
         className="items-stretch"
       >
