0ff18f6c36df9f7359c1793a6dcea0cc5ce20a8f
SoaBra <soabrago@gmail.com>
2026-06-26 10:01:01 +0300

    Load root connector anchor styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './features/planning/elements/smart/root-connector.css'
 import { Telemetry } from './infra/telemetry'
 
 Telemetry.init();
